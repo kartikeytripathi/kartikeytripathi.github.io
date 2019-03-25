5ef750d9c1103c2b4668ecbf583e94ba41705887
kartikeytripathi <kartikey.tripathi.37@gmail.com>
2019-03-25 19:13:16 +0530

    Add Title Section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ $(document).ready(function () {
     pagination: false,
     animation: 'fade',
     play: 5000
-  })
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Student.", "Web Developer."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 
 });
